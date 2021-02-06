589e873f40b365f7f43e6beabf678818524528ae
sidds4474 <siddy5691@gmail.com>
2021-02-06 16:12:11 +0530

    final

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ async function runServer() {
 
   server.use('/api/v1/projects', require('./routes/projects'));
 
+  server.get('/test', (req, res) => {
+    res.json({message: 'Hello World'});
+  })
+
+  // server.get('*', (req, res) => {
+  //   res.json({message: 'Welcome to Portfolio api backend...'});
+  // })
+
+  server.get('', (req, res) => {
+    res.sendFile('index.html', {root:__dirname});
+  })
+
+
   const PORT = parseInt(process.env.PORT, 10) || 3002;
   server.listen(PORT, (err) => {
     if (err) console.error(err);
